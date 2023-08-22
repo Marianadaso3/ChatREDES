37c4caa3e607921237793e2fbd02c5634c83305e
Marianadaso3 <72479030+Marianadaso3@users.noreply.github.com>
2023-08-22 02:52:53 -0600

    Creating states and format

--- src/index.js
+++ src/index.js
@@ -36,5 +36,79 @@ function menu() {
   })
 }
 
+// Funcion para cambiar el estado y show del usuario
+const cambiarEstadoUsuario = (xmpp, show, status) => {
+  try {
+    const presenceStanza = xml(
+      'presence',
+      {},
+      xml('show', {}, show), // estado como 'chat', 'away', 'dnd', etc.
+      xml('status', {}, status) // mensaje opcional, por ejemplo, "En una reunión"
+    )
+
+    xmpp.send(presenceStanza)
+    console.log(`💭 Estado cambiado a ${show} con status ${status}`)
+  } catch (error) {
+    console.error(`❌ Error al cambiar el estado y show del usuario: ${error.message}`)
+  }
+}
+
+// Funcion para obtener el roster del usuario
+const getRoster = (xmpp,jid) => {
+  const rosterQuery = xml('iq', { type: 'get', to:`${jid}@alumchat.xyz`}, xml('query', { xmlns: 'jabber:iq:roster' }))
+  xmpp.send(rosterQuery)
+}
+
+// Funcion para limpiar el roster al cerrar sesion
+const cleanContacts = () => {
+  for (const contact in contacts) {
+    delete contacts[contact]
+  }
+}
+
+// Funcion para dar formato a los contactos al mostrarlos en el CLI
+const formatContacts = async () => {
+  if (contacts.length === 0) {
+    console.log('No tienes contactos')
+  }else{
+    
+    console.log('Contactos:') 
+    console.log('\tJID    \t Show    \t Estado')
+    for (const contact in contacts) {
+      const isGroup = contact.includes('@conference.alumchat.xyz')
+      const contactJid = contact.split('@')[0]
+
+      if (isGroup) {
+        // Obtener el rouster del group y mostrarlo
+        
+        const grupRost = groupRoster[contact]
+        console.log(`=> ${contactJid}: ${Object.keys(grupRost).length} miembros`)
+        if (grupRost) {
+          for (const contact in grupRost) {
+            const contactJid = contact.split('@')[0]
+            console.log(`\t--> ${contactJid}: ${grupRost[contact].show } \t(${grupRost[contact].status? grupRost[contact].status : 'sin estado'})`)
+          }
+        }
+        continue
+      }
+
+      //print sin tanbulacion
+      if (contactJid.length > 10) {
+        console.log(`=> ${contactJid}: ${contacts[contact].show } \t(${contacts[contact].status? contacts[contact].status : 'sin estado'})`)
+      }else if(contactJid.length < 7){
+        console.log(`=> ${contactJid}:\t\t ${contacts[contact].show } \t(${contacts[contact].status? contacts[contact].status : 'sin estado'})`)
+  
+      }
+      
+      else{
+        console.log(`=> ${contactJid}:\t ${contacts[contact].show } \t(${contacts[contact].status? contacts[contact].status : 'sin estado'})`)
+  
+      }
+    }
+  }
+}
+
+
+
 
 menu()
